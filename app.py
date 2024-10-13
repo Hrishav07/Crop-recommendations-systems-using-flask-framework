1714ad0baf85ae072f60fa40a76c764ba4c51a0a
ELLY BETT <bettelly84@gmail.com>
2024-10-13 19:32:30 +0300

    weather update

--- app.py
+++ app.py
@@ -1,18 +1,15 @@
-from flask import Flask, render_template, request, jsonify  # Use request from Flask
+from flask import Flask, request, render_template
 import numpy as np
 import pickle
-import datetime
-import requests  # For making API calls
+import requests
 
-# Importing model
-model = pickle.load(open('model.pkl', 'rb'))
-sc = pickle.load(open('standscaler.pkl', 'rb'))
-ms = pickle.load(open('minmaxscaler.pkl', 'rb'))
+# importing model
+model = pickle.load(open('model.pkl','rb'))
+sc = pickle.load(open('standscaler.pkl','rb'))
+ms = pickle.load(open('minmaxscaler.pkl','rb'))
 
-# Creating Flask app
+# creating flask app
 app = Flask(__name__)
-
-API_KEY = ''
 
 @app.route('/')
 def index():
@@ -67,57 +64,36 @@ def predict():
 
     if prediction[0] in crop_dict:
         crop = crop_dict[prediction[0]]
-        image_file = image_dict[crop]
-        result = f"{crop} is the best crop to be cultivated here."
+        image_file = image_dict[crop]  # Get the corresponding image file
+        result = "{} is the best crop to be cultivated right there.".format(crop)
     else:
-        result = "Sorry, I can't provide prediction on the provided data."
+        result = "Sorry, we could not determine the best crop to be cultivated with the provided data."
 
     return render_template('index.html', result=result, image=image_file)
-
-@app.route('/historical_data', methods=['POST'])
-def historical_data():
+@app.route('/realtime_weather', methods=['POST'])
+def realtime_weather():
     lat = request.form['lat']
     lon = request.form['lon']
-    start_date = request.form['start_date']
-    end_date = request.form['end_date']
+    api_key = 'c69014a31226004f6f7759c671c56432'
+    url = f'http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=metric'
 
-    # Convert dates to UNIX timestamps
-    start_timestamp = int(datetime.datetime.strptime(start_date, "%Y-%m-%d").timestamp())
-    end_timestamp = int(datetime.datetime.strptime(end_date, "%Y-%m-%d").timestamp())
+    response = requests.get(url)
+    weather_data = response.json()
 
-    historical_data = {
-        "temperature": [],
-        "humidity": [],
-        "rainfall": []
-    }
+    if weather_data['cod'] == 200:  # Check if the request was successful
+        temperature = weather_data['main']['temp']
+        humidity = weather_data['main']['humidity']
+        description = weather_data['weather'][0]['description']
+        wind_speed = weather_data['wind']['speed']
 
-    current_timestamp = start_timestamp
-    while current_timestamp <= end_timestamp:
-        api_url = (
-            f"https://api.openweathermap.org/data/3.0/onecall/timemachine?"
-            f"lat={lat}&lon={lon}&dt={current_timestamp}&appid={API_KEY}&units=metric"
-        )
-
-        response = requests.get(api_url)
-        if response.status_code == 200:
-            data = response.json()
-            current_weather = data.get('current', {})
-
-            # Safely get weather values with defaults
-            temp = current_weather.get('temp', 'N/A')
-            humidity = current_weather.get('humidity', 'N/A')
-            rain = current_weather.get('rain', {}).get('1h', 0)
-
-            historical_data["temperature"].append(temp)
-            historical_data["humidity"].append(humidity)
-            historical_data["rainfall"].append(rain)
-        else:
-            print(f"API call failed with status code {response.status_code}")
-            return jsonify({'error': 'Failed to fetch historical data'}), 500
-
-        current_timestamp += 86400  # Move to the next day
-
-    return render_template('index.html', historical_data=historical_data)
+        return render_template('index.html', weather_data={
+            'temperature': temperature,
+            'humidity': humidity,
+            'description': description,
+            'wind_speed': wind_speed
+        }, result=None)  # Return to the same template with weather data
+    else:
+        return render_template('index.html', error="Failed to get weather data.", result=None)
 
 if __name__ == "__main__":
     app.run(debug=True)
